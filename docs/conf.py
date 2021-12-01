f726aedea56283b15995102381a07053d234971f
Matt Kasfeldt <matt.kasfeldt@gmail.com>
2021-12-01 12:11:00 -0800

    update docs

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ release = '0.0.1'
 # ones.
 extensions = ['sphinx.ext.autodoc']
 source_suffix = '.rst'
-master_doc = 'index'
+master_doc = 'source/index'
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
