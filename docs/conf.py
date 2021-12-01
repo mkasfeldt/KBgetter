55827f8622663ba852b3c1219c8189198b14b949
Matt Kasfeldt <matt.kasfeldt@gmail.com>
2021-12-01 11:43:01 -0800

    docs update

--- docs/conf.py
+++ docs/conf.py
@@ -1,32 +1,55 @@
-# -*- coding: utf-8 -*-
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-import sys
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
 import os
+import sys
+sys.path.insert(0, os.path.abspath('../../KBGetter'))
 
-sys.path.insert(0, os.path.abspath('extensions'))
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage',
-              'sphinx.ext.viewcode', 'sphinx.ext.intersphinx',
-              'sphinx.ext.todo', 'sphinx.ext.extlinks']
+# -- Project information -----------------------------------------------------
 
-todo_include_todos = True
-templates_path = ['_templates']
+project = 'FSGetter'
+copyright = '2021, Matt Kasfeldt'
+author = 'Matt Kasfeldt'
+
+# The full version, including alpha/beta/rc tags
+release = '0.0.1'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = ['sphinx.ext.autodoc']
 source_suffix = '.rst'
 master_doc = 'index'
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
-add_function_parentheses = True
-#add_module_names = True
-# A list of ignored prefixes for module index sorting.
-#modindex_common_prefix = []
 
-project = u'KBgetter'
-copyright = u'2021, Matt Kasfeldt'
 
-version = ''
-release = ''
+# -- Options for HTML output -------------------------------------------------
 
-sys.path.append(os.path.abspath('_themes'))
-html_theme_path = ['_themes']
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
 html_theme = 'sphinx_rtd_theme'
 
-todo_include_todos = True
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
